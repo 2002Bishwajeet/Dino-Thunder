c9902c7a1dbb0876e93aacf0f80fd54f5f9e9c5f
Amisha <91381210+oceanofamisha@users.noreply.github.com>
2021-10-26 23:23:08 +0530

    change resolution

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(600, 400), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
